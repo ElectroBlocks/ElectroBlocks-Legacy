e83c5a66847d49479bfa394ca196c11a68ae11c6
Noah Glaser <glaserpower@gmail.com>
2019-10-26 23:10:39 -0700

    removed global state with function getting ready for temp

--- src/app/core/services/blockly/generators/temp.ts
+++ src/app/core/services/blockly/generators/temp.ts
@@ -1,18 +1,15 @@
 import * as Blockly from 'blockly/core';
 import { Block } from 'blockly';
 
-const tempPinContainer = { tempPin: 7 };
-
 Blockly.Arduino['temp_setup'] = function(block: Block) {
-  tempPinContainer.tempPin = parseInt(block.getFieldValue('PIN')) || 7;
-
   Blockly.Arduino.libraries_['temp_setup'] = '#include <dht.h>;\n dht DHT;\n';
 
-  return '';
-};
+  Blockly.Arduino.functionNames_['takeTempReading'] = `
+void takeTempReading() {
+  DHT.read11(${block.getFieldValue('PIN')});
+}`;
 
-Blockly.Arduino['temp_read_temp_humidity'] = function() {
-  return 'DHT.read11(' + tempPinContainer.tempPin + ');\n';
+  return '';
 };
 
 Blockly.Arduino['temp_get_temp'] = function() {
